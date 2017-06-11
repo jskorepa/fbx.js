92497c280aa75fa27f885cb4c3ae6cf9f3990bf6
Jakub Skořepa <jakub@skorepa.info>
2017-06-12 00:24:09 +0200

    Deduplicate everything except models

--- src/bin/fbxcopy.ts
+++ src/bin/fbxcopy.ts
@@ -37,8 +37,8 @@ const argValue = (arg: string) => {
             if(prop.type !== 'L') throw new Error('Invalid FBX file')
             return prop.value
         }
-        if(hasArg('--deduplicate-materials') || hasArg('-m')) {
-            console.time('deduplicate-materials')
+        if(hasArg('--deduplicate') || hasArg('-d')) {
+            console.time('deduplicate')
             const idReplaceMap: {
                 [low: number]: {
                     [high: number]: { low: number, high: number },
@@ -48,27 +48,30 @@ const argValue = (arg: string) => {
             const objectsNode = parsed.children.filter(a => a.name === 'Objects')[0]
             if(!objectsNode.children) throw new Error('No Objects node in fbx file')
             const objects = objectsNode.children || []
-            const materials = objects.filter(a => a.name === 'Material') || []
-            const materialJSON = materials.map(o => JSON.stringify(o.children))
-            for(let i = 0; i < materials.length; i++) {
-                const uid = getUID(materials[i])
-
-                // find same material
-                for(let j = 0; j < i; j++) {
-                    if(materialJSON[i] === materialJSON[j]) {
-                        if(!idReplaceMap[uid.low]) idReplaceMap[uid.low] = {}
-                        idReplaceMap[uid.low][uid.high] = getUID(materials[j])
-                        break
+            const objectsJSON = objects.map(o => JSON.stringify(o.children))
+            for(let i = 0; i < objects.length; i++) {
+                try {
+                    if(objects[i].name === 'Model') continue
+                    const uid = getUID(objects[i])
+                    for(let j = 0; j < i; j++) {
+                        if(objectsJSON[i] === objectsJSON[j]) {
+                            if(!idReplaceMap[uid.low]) idReplaceMap[uid.low] = {}
+                            idReplaceMap[uid.low][uid.high] = getUID(objects[j])
+                            break
+                        }
                     }
-                }
+                } catch(e) {}
             }
 
             objectsNode.children = objects.filter(o => {
-                if(o.name !== 'Material') return true // only filter materials
-                const uid = getUID(o)
-                if(!idReplaceMap[uid.low]) return true
-                if(!idReplaceMap[uid.low][uid.high]) return true
-                return false
+                try {
+                    const uid = getUID(o)
+                    if(!idReplaceMap[uid.low]) return true
+                    if(!idReplaceMap[uid.low][uid.high]) return true
+                    return false
+                } catch(e) {
+                    return true
+                }
             })
 
             const connections = parsed.children.filter(a => a.name === 'Connections')[0]
@@ -87,7 +90,7 @@ const argValue = (arg: string) => {
 
                 return con
             })
-            console.timeEnd('deduplicate-materials')
+            console.timeEnd('deduplicate')
         }
         console.time('serialize')
         const serialized = await fbx.serialize(parsed)
